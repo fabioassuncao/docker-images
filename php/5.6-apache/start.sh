5de6601a2e55cd495895acbfece308684a96279d
Fábio Assunção <fabio23gt@gmail.com>
2018-02-23 03:07:18 -0300

    Update Dockerfile PHP:5.6-apache

--- php/5.6-apache/start.sh
+++ php/5.6-apache/start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -e
 
-if [ "$APACHE_ENVVARS" ]; then
-	source "$APACHE_ENVVARS"
-fi
-
-# Apache gets grumpy about PID files pre-existing
-: "${APACHE_PID_FILE:=${APACHE_RUN_DIR:=/var/run/apache2}/apache2.pid}"
-rm -f "$APACHE_PID_FILE"
-
-# Starts apache2!
-exec apache2 -D FOREGROUND "$@"
+# Starts apache2
+exec /usr/sbin/apache2ctl -D FOREGROUND "$@"
