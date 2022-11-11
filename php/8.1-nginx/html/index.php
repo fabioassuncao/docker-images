50fada77533c7d04eac87fbb8ac9cf8f55526a18
Fábio Assunção <fabio23gt@gmail.com>
2022-11-11 03:14:20 -0300

    Added support for PHP 8.1

--- php/8.1-nginx/html/index.php
+++ php/8.1-nginx/html/index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
         <h4>Congratulations!</h4>
-        <p>You have successfully deployed a <strong>docker</strong> container running our <strong>NGINX</strong> with <strong>PHP-FPM 7.x</strong> image</p>
+        <p>You have successfully deployed a <strong>docker</strong> container running our <strong>NGINX</strong> with <strong>PHP-FPM 8.x</strong> image</p>
         <p><strong>NGINX: </strong>v<?php echo $_ENV['NGINX_VERSION'] ?><br><strong>PHP-FPM: </strong>v<?php echo phpversion(); ?><br><strong>LOADED CONFIG: </strong><?php echo php_ini_loaded_file(); ?><br><strong>WEB ROOT: </strong><?php echo $_ENV['DOCUMENT_ROOT'] ?><br><strong>HOSTNAME: </strong><?php echo gethostname(); ?><br></p>
 </body>
 </html>
